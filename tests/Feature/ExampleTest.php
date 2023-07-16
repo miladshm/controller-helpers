a36eec4d730a8986ae5a39f13a9bf212dd789e74
milad <mldsh6@gmail.com>
2023-07-16 23:06:36 +0330

    add phpunit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Miladshm\ControllerHelpers\Tests\Feature;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
