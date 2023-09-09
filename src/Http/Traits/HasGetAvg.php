2939232e41e27d98bc6cf9963b2c8e813bc99f44
milad <mldsh6@gmail.com>
2023-09-09 15:19:07 +0330

    Get Average of a column

---
+++ src/Http/Traits/HasGetAvg.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Miladshm\ControllerHelpers\Http\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Schema;
+use Miladshm\ControllerHelpers\Libraries\Responder\Facades\ResponderFacade;
+use Miladshm\ControllerHelpers\Traits\WithFilters;
+use Miladshm\ControllerHelpers\Traits\WithModel;
+
+trait HasGetAvg
+{
+    use WithModel, WithFilters;
+
+    /**
+     * @param string $column
+     * @return JsonResponse
+     */
+    public function getAvg(string $column): JsonResponse
+    {
+        $avg = Schema::connection($this->model()->getConnectionName())->hasColumn($this->model()->getTable(), $column)
+            ? $this->model()
+                ->query()
+                ->when(true, function (Builder $builder) {
+                    return $this->filters($builder);
+                })
+                ->avg($column)
+            : 0;
+        return ResponderFacade::setData(compact('avg'))->respond();
+
+    }
+}
